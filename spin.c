e0b8ef8233a844463468f01569c9812ebdc7d4bb
Naveen Anand <naveenanand.spark@gmail.com>
2014-03-17 16:35:51 -0500

    Testing spin.c push

--- spin.c
+++ spin.c
@@ -19,19 +19,45 @@ static inline uint xchg(volatile unsigned int *addr, unsigned int newval)
     	return result;
 }
 
+void init()
+{
+	lock=0;
+}
+
+void spin_lock()
+{
+	while(xchg(&lock,1));
+}
+
+
+void spin_unlock()
+{
+	xchg(&lock,0);
+}
+
+
 void *increment(void *n)
 {
 	int i,count=(int)n;
 	printf("Count = %d\n",count);
 	for(i=0;i<count;i++)
+	{	
+		spin_lock();
 		balance++;
-
+		spin_unlock();
+	}
 	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
 	pthread_t thread1,thread2;
+	if(argc<2)
+	{
+		printf("Error; Usage: ./spin.o <count>\n");
+		return -1;
+	}
+
 	int n=atoi(argv[1]);
 
 	printf("Balance = %d\n",balance);
