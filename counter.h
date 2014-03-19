367f2c11e8868578e50ab38411e53d73370b078a
Naveen Anand <naveenanand.spark@gmail.com>
2014-03-18 22:08:50 -0500

    Counter Functions added

---
+++ counter.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Counter_Init(counter_t *c, int value);
+int Counter_GetValue(counter_t *c);
+void Counter_Increment(counter_t *c);
+void Counter_Decrement(counter_t *c);
